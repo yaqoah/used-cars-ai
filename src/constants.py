12d5297fb46b891cae75f1fb989c3380760d4a69
Ahmed Awad <balotelli-7amood@hotmail.com>
2025-05-05 14:21:09 +0400

    Initial commit

---
+++ src/constants.py
@@ -0,0 +1,34 @@
+CHROMEDRIVER = "C:/Users/Balot/chromedriver"
+URL1 = "https://www.yesauto.co.uk/buy/cars/diesel/automatic?distance=national&postcode=SA1%201LA&pageIndex={}&hasFilter=1&hasSort=1&vehicleage=under-2015"
+URL2 = "https://www.yesauto.co.uk/buy/cars/diesel/automatic?distance=national&postcode=SA1%201LA&pageIndex={}&hasFilter=1&hasSort=1&vehicleage=above-2015"
+URL3 = "https://www.yesauto.co.uk/buy/cars/petrol/automatic?distance=national&postcode=SA1%201LA&pageIndex={}&hasFilter=1&hasSort=1&vehicleage=under-2017"
+URL4 = "https://www.yesauto.co.uk/buy/cars/petrol/automatic?distance=national&postcode=SA1%201LA&pageIndex={}&hasFilter=1&hasSort=1&vehicleage=above-2017"
+URL5 = "https://www.yesauto.co.uk/buy/cars/diesel/manual?distance=national&postcode=SA1%201LA&pageIndex={}&hasFilter=1&hasSort=1&vehicleage=under-2015"
+URL6 = "https://www.yesauto.co.uk/buy/cars/diesel/manual?distance=national&postcode=SA1%201LA&pageIndex={}&hasFilter=1&hasSort=1&vehicleage=above-2015"
+URL7 = "https://www.yesauto.co.uk/buy/cars/petrol/manual?distance=national&postcode=SA1%201LA&pageIndex={}&hasFilter=1&hasSort=1&vehicleage=under-2015"
+URL8 = "https://www.yesauto.co.uk/buy/cars/petrol/manual?distance=national&postcode=SA1%201LA&pageIndex={}&hasFilter=1&hasSort=1&vehicleage=above-2015"
+URL9 = "https://www.yesauto.co.uk/buy/cars/semi-automatic?distance=national&postcode=SA1%201LA&pageIndex={}&hasFilter=1&hasSort=1"
+URL10 = "https://www.yesauto.co.uk/buy/cars/hybrid-dieselelectric_electric_hybrid-petrolelectric_hybrid-petrolelectric-plug-in_bifuel?distance=national&postcode=SA1%201LA&pageIndex={}&hasFilter=1&hasSort=1"
+PAGES1 = 315
+PAGES2 = 417
+PAGES3 = 337
+PAGES4 = 417
+PAGES5 = 417
+PAGES6 = 417
+PAGES7 = 417
+PAGES8 = 417
+PAGES9 = 75
+PAGES10 = 204
+COLUMNS = ["make", "model", "year", "body", "mileage", "fuel", "transmission", "engineSize", "enginePower", "price"]
+MAX_PAGES = 417
+BODY_TYPES = ["convertible", "coupe", "estate", "hatchback",
+              "suv", "pick-up", "saloon", "mpv", "van", "4x4", "crew van"
+              "panel van", "limousine", "dropside", "cabriolet/roadster",
+              "cabriolet", "station wagon"]
+TRANSMISSION_TYPES =["Automatic", "Manual", "Semi Automatic", "CVT"]
+FUEL_TYPES =["Petrol", "Diesel", "Electric"]
+PRICE_CLASS = "CarItemNew_priceLeft__2DyV7 "
+PRICE_CSS = ".CarItemNew_price__2RvdR"
+AD_NAME_CLASS = "CarItemNew_carname__3KzHx"
+ATTRIBUTES_CLASS = "CarItemNew_paramsWrap__1nr67 CarItemNew_paramsWrapUk__1UHqJ"
+NEXT_CSS = ".MuiButtonBase-root.MuiPaginationItem-root.MuiPaginationItem-page"
